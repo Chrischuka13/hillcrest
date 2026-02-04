410784bc02a04f4b030fa7809d04fd62a6f7bcdd
Chrischuka13 <divineprovidence30@gmail.com>
2026-02-04 13:31:33 +0100

    rename team

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from '../../components/NavBar'
 import Hero from './Hero'
-import Team from './team'
+import Team from './Team'
 import CTA from '../../components/CTA'
 import Footer from '../../components/Footer'
 
